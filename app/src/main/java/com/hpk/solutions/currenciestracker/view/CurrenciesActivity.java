87d1e4c2d5e7ebd7f929321afd9064b4069ef1a8
Jakub Gugała <jakub.gugala@gmail.com>
2018-08-20 19:00:05 +0200

    Application class added in AndroidManifest.xml, Dagger initialized in the main activity, the application name changed.

--- app/src/main/java/com/hpk/solutions/currenciestracker/view/CurrenciesActivity.java
+++ app/src/main/java/com/hpk/solutions/currenciestracker/view/CurrenciesActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 
 import com.hpk.solutions.currenciestracker.R;
 
+import dagger.android.AndroidInjection;
+
 public class CurrenciesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currencies);
     }

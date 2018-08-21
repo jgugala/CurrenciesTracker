01e33cb09817d01e2e01c4073c40f8a2595704b2
Jakub Gugała <jakub.gugala@gmail.com>
2018-08-21 18:10:09 +0200

    CurrencyDetailsActivity created with basic layout, registered in the manifest.

---
+++ app/src/main/java/com/hpk/solutions/currenciestracker/view/CurrencyDetailsActivity.java
@@ -0,0 +1,25 @@
+package com.hpk.solutions.currenciestracker.view;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hpk.solutions.currenciestracker.R;
+
+import dagger.android.AndroidInjection;
+
+/**
+ * Created by $USER_NAME on 21.08.18.
+ */
+public class CurrencyDetailsActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TICKER_SYMBOL = "extra_ticket_symbol";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
+        super.onCreate(savedInstanceState);
+        DataBindingUtil.setContentView(this, R.layout.activity_currency_details);
+    }
+}
